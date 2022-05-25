66fe882fc8ac50266f3c42b160d7ce4065d3da1c
matkucz <mat.kuczyn@gmail.com>
2022-05-25 23:23:18 +0200

    Account type in django-admin user creation form. Size model added to django-admin.

--- imgocean/ocean/admin.py
+++ imgocean/ocean/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, Account
-from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import User, Account, Size
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    dorm = CustomUserChangeForm
-    model = User
-
-admin.site.register(User, CustomUserAdmin)
 admin.site.register(Account)
+admin.site.register(User)
+admin.site.register(Size)
